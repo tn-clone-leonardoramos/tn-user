1ad851d2cf4cb3add13a2e32d0ea310cce7bd3df
Leonardo Ramos <leorrc96@gmail.com>
2024-10-25 13:59:44 +0000

    chore: deploy only if tests passed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ import { AppService } from '@app/app.service'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/healthcheck')
   healthCheck() {
     return
   }
